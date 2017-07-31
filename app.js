f92e2370b182a753470d950aa7939f97a5126d14
Emily Lemmon <eclaire.lemmon@gmail.com>
2017-07-31 10:41:35 -0500

    separate factory

---
+++ app.js
@@ -0,0 +1,3 @@
+'use strict';
+
+const mushroomApp = angular.module('shroomApp', []);
--- mushroomController.js
+++ mushroomController.js
@@ -1,22 +1,5 @@
 'use strict';
 
-const mushroomApp = angular.module('shroomApp', []);
-
-mushroomApp.factory("MushroomFactory", function($q, $http) {
-	function getShrooms() {
-		return $q( (resolve, reject) => {
-			$http.get('./mushrooms.json')
-			.then( (shrooms) => {
-				console.log("shrooms", shrooms.data.mushrooms);
-				resolve(shrooms.data.mushrooms);
-			})
-			.catch( (err) => {
-				reject(err);
-			});
-		});
-	}
-	return { getShrooms };
-});
 
 mushroomApp.controller("MushroomController", function($scope, MushroomFactory) {
 	MushroomFactory.getShrooms()
